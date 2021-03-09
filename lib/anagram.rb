97fae62d594e747b6a348cd8b8fc892fd1fd24f9
Sam D <samidiaz3@gmail.com>
2021-03-09 03:56:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+
+
+    
+    #return all matches or empty array
+
+  end
+
+
+
+
+end
